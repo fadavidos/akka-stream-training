4e6e62fc455c4e0af40ac078ede0433c205dc879
Fabian Osorio <fadavidos@gmail.com>
2023-01-16 21:01:18 -0500

    introduction

--- src/main/scala/MainIntroduction.scala
+++ src/main/scala/MainIntroduction.scala
@@ -34,7 +34,6 @@ object MainIntroduction extends App {
     Tweet(Author("appleman"), System.currentTimeMillis, "#apples rock!") ::
     Tweet(Author("drama"), System.currentTimeMillis, "we compared #apples to #oranges!") ::
     Nil
-  )
 
   implicit val system: ActorSystem = ActorSystem.create("introduction")
 
